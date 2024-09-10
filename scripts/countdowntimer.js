3632cf94fae98fce41e2c663d9a3f1486b5d2d5b
mikki <mkdubik@gmail.com>
2024-09-10 23:04:47 +0000

    i18n translations

---
+++ scripts/countdowntimer.js
@@ -0,0 +1,34 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jul 26, 2025 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  var daysText = days === 1 ? i18n[currentLanguage]["day"] : i18n[currentLanguage]["days"]
+  var hoursText = hours === 1 ? i18n[currentLanguage]["hour"] : i18n[currentLanguage]["hours"]
+  var minutesText = minutes === 1 ? i18n[currentLanguage]["minute"] : i18n[currentLanguage]["minutes"]
+  var secondsText = seconds === 1 ? i18n[currentLanguage]["second"] : i18n[currentLanguage]["seconds"]
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + " " + daysText + " "+ hours + " " + hoursText + " "
+  + minutes + " " + minutesText + " " + seconds + " " + secondsText;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = i18n[currentLanguage]["countdown-finished"];
+  }
+}, 1000);
---
+++ scripts/i18n.js
@@ -0,0 +1,185 @@
+const i18n = {
+	"is": {
+    	"schedule": "Dagskrá",
+        "ceremony": 'Athöfnin verður í <a href="https://www.google.com/maps?ll=64.125268,-20.524612&z=15&t=h&hl=en&gl=IS&mapclient=embed&cid=2354147562176909985" target="_blank">Skálholtskirkju</a>',
+        "dinner": 'Við munum skála í <a href="https://maps.app.goo.gl/qfYUDDLa7vxXxydf7" target="_blank">Eyvindartungu</a>',
+        "ceremony-events": "Athöfn - 15:00 - Skálholtskirkja",
+        "predrinks-events": "Fordrykkur - 17:00 - Eyvindartunga",
+        "dinner-events": "Matur - 18:30 - Eyvindartunga",
+        "cake-events": "Kaka - 21:00 - Eyvindartunga",
+        "dance-events": "Dansiball - 22:30 - Eyvindartunga",
+        "days": "dagar",
+        "day": "dagur",
+        "hours": "klukkustundir",
+        "hour": "klukkustund",
+        "minutes": "mínútur",
+        "minute": "mínúta",
+        "seconds": "sekúndur",
+        "second": "sekúnda",
+        "countdown-finished": "Brúðkaupið er búið :)",
+        "submitting": "Er að senda svar..",
+        "missing-form-data": "Vantar upplýsingar fyrir svar. Fylltu inn Nafn, Símanúmer, Tölvupóstfang, Heimilisfang og lagið sem kemur þér á dansgólfið. Reyndu svo aftur.",
+        "name-conflict": "Nafn er nú þegar skráð. Hafðu samband við okkur ef það þarf að breyta skráninguni eða ef þú kannast ekki við skráninguna",
+        "unknown-error": "Eitthvað hefur farið úrskeiðis. Hafðu samband við okkur svo að hægt sé að skrá þig",
+        "accepted": "Svar skráð. Við hlökkum til að sjá þig á deginum okkar",
+        "rejected": "Svar skráð. Okkur þykir það leitt að þú kemst ekki",
+        "Name": "Nafn",
+        "Phone": "Símanúmer",
+        "Email": "Tölvupóstfang",
+        "Address": "Heimilisfang",
+        "Food": "Ofnæmisvaldar / Mataræði",
+        "Music": "Hvaða lag kemur þér á dansgólfið ?",
+        "accept-button": "Já ég mæti",
+        "decline-button": "Því miður, ég get ekki mætt",
+        "rsvp-submit-button": "Senda svar",
+        "extra-details-head": "Auka upplýsingar",
+        "extra-details-1": "Ef veður leyfir er hægt að tjalda á tjaldsvæði Eyvindartungu fyrir þá sem hafa áhuga á því",
+        "extra-details-2": "Einnig er hægt að bóka herbergi í gegnum bókunarsíður",
+        "gifts-head": "Gjafir",
+        "gifts-details-1": "Við þökkum fyrir áhugan á að gefa okkur brúðkaupsgjöf",
+        "gifts-details-2": "Það sem væri hentugast fyrir okkur væri að fá gjöf í formi styrks",
+        "gifts-details-3": "Leggið vinsamlegast á Kennitölu: 010190-2159 og bankanúmer 0306-26-135864"
+    },
+	"en": {
+    	"schedule": "Schedule",
+        "ceremony": 'The ceremony will be in <a href="https://www.google.com/maps?ll=64.125268,-20.524612&z=15&t=h&hl=en&gl=IS&mapclient=embed&cid=2354147562176909985" target="_blank">Skálholtskirkja</a>',
+        "dinner": 'The dinner will be in <a href="https://maps.app.goo.gl/qfYUDDLa7vxXxydf7" target="_blank">Eyvindartunga</a>',
+        "ceremony-events": "Ceremony - 15:00 - Skálholtskirkja",
+        "predrinks-events": "Predrinks - 17:00 - Eyvindartunga",
+        "dinner-events": "Dinner - 18:30 - Eyvindartunga",
+        "cake-events": "Cake - 21:00 - Eyvindartunga",
+        "dance-events": "Dance party - 22:30 - Eyvindartunga",
+        "days": "days",
+        "day": "day",
+        "hours": "hours",
+        "hour": "hour",
+        "minutes": "minutes",
+        "minute": "minute",
+        "seconds": "seconds",
+        "second": "second",
+        "countdown-finished": "The wedding is over :)",
+        "submitting": "Submitting..",
+        "missing-form-data": "Missing information. Please fill in Name, Phone number, E-mail, Address and the song that gets you to the dance floor and then retry.",
+        "name-conflict": "Name already registered. Please get in touch with us if you need to change the registration",
+        "unknown-error": "Something went wrong. Please get in touch with us to register your answer",
+        "accepted": "Answer submitted. <TODO>",
+        "rejected": "Answer submitted. <TODO BAD>",
+        "Name": "Name",
+        "Phone": "Phone number",
+        "Email": "E-mail",
+        "Address": "Address",
+        "Food": "Dietary restrictions",
+        "Music": "Which tune gets you on the dance floor ?",
+        "accept-button": "See you there!",
+        "decline-button": "I cannot attend",
+        "rsvp-submit-button": "Submit",
+        "extra-details-head": "Details",
+        "extra-details-1": "If weather permits <todo>",
+        "extra-details-2": "ALSO its possible <todo>",
+        "gifts-head": "Gifts",
+        "gifts-details-1": "gifts-details-1 <todo>",
+        "gifts-details-2": "gifts-details-2 <todo>",
+        "gifts-details-3": "gifts-details-3 <todo>"
+    },
+	"pl": {
+    	"schedule": "Rozkład",
+        "ceremony": 'Ceremonia bedzie w kosciele <a href="https://www.google.com/maps?ll=64.125268,-20.524612&z=15&t=h&hl=en&gl=IS&mapclient=embed&cid=2354147562176909985" target="_blank">Skálholtskirkja</a>',
+        "dinner": 'Wesele bedzie w <a href="https://maps.app.goo.gl/qfYUDDLa7vxXxydf7" target="_blank">Eyvindartunga</a>',
+        "ceremony-events": "Ceremonia - 15:00 - Skálholtskirkja",
+        "predrinks-events": "Drinki - 17:00 - Eyvindartunga",
+        "dinner-events": "Kolacja - 18:30 - Eyvindartunga",
+        "cake-events": "Ciasto - 21:00 - Eyvindartunga",
+        "dance-events": "Party - 22:30 - Eyvindartunga",
+        "days": "dni",
+        "day": "dzien",
+        "hours": "godziny",
+        "hour": "godzina",
+        "minutes": "minuty",
+        "minute": "minuta",
+        "seconds": "sekundy",
+        "second": "sekunda",
+        "countdown-finished": "Wesele sie szkonczylo :)",
+        "submitting": "Wysylam..",
+        "missing-form-data": "Brak danych. <TODO>",
+        "name-conflict": "Imie <TODO>",
+        "unknown-error": "Cos nie tak wyszlo <TODO>",
+        "accepted": ":)",
+        "rejected": ":(",
+        "Name": "Imie",
+        "Phone": "Numer telefonu",
+        "Email": "E-mail",
+        "Address": "Adres",
+        "Food": "Alergie lub specialna dieta",
+        "Music": "Jaka piosenka cie <TODO> ?",
+        "accept-button": "Tak",
+        "decline-button": "Niestety",
+        "rsvp-submit-button": "Wyslac",
+        "extra-details-head": "Dodatkowe informacje",
+        "extra-details-1": "Jezeli pogoda <todo>",
+        "extra-details-2": "Wyszukiwarka <todo>",
+        "gifts-head": "Prezenty",
+        "gifts-details-1": "prezenty 1",
+        "gifts-details-2": "prezenty 2",
+        "gifts-details-3": "prezenty 3"
+    },
+};
+
+var currentLanguage = "is"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const isButton = document.getElementById("is")
+    isButton.addEventListener("click", function() {
+        if (currentLanguage != "is") {
+            translatePage("is")
+        }
+    });
+
+    const enButton = document.getElementById("en")
+    enButton.addEventListener("click", function() {
+        if (currentLanguage != "en") {
+            translatePage("en")
+        }
+    });
+
+    const plButton = document.getElementById("pl")
+    plButton.addEventListener("click", function() {
+        if (currentLanguage != "pl") {
+            translatePage("pl")
+        }
+    });
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const lang = urlParams.get('lang')
+
+    switch (lang) {
+        case "en":
+            currentLanguage = "en"
+            enButton.focus()
+            break;
+        case "pl":
+            currentLanguage = "pl"
+            plButton.focus()
+            break;
+        default:
+            currentLanguage = "is"
+            isButton.focus()
+            break
+    }
+
+
+    translatePage(currentLanguage)
+});
+
+function translatePage(language) {
+    const translate = document.getElementsByClassName("translate");
+
+    for (let i = 0; i < translate.length; i++) {
+        if (translate[i].nodeName === "INPUT") {
+            translate[i].placeholder = i18n[language][translate[i].name]
+        } else {
+            translate[i].innerHTML = i18n[language][translate[i].id]
+        }
+    }
+
+    currentLanguage = language
+}
