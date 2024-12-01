93a3a87abfb1c655a0bf031ce76525bd66e0da0b
mikki <mkdubik@gmail.com>
2024-12-01 21:14:22 +0000

    smooth scrolling fixed

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,3 @@ $(document).ready(function() {
     // once: true
   }); // initialize animate on scroll library
 });
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            block: 'start',
-            inline: 'nearest',
-            behavior: 'smooth'
-        });
-    });
-});
